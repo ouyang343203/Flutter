63028e9109d1045a7b0a068f6fb6c116f4e7f067
yangwen <ouy15818731124@gmail.com>
2023-04-10 08:52:09 +0800

    更新-函数

--- dart/demo03-函数/day01函数.dart
+++ dart/demo03-函数/day01函数.dart
@@ -43,10 +43,46 @@ void main() {
   String printuserinfowith(String username, int age) {
     return '姓名: $username----$age';
   }
+
   printuserinfowith('章三', 30);
 
-//定义可选参数的函数 注意可选参数放在末尾用 [参数1，参数2，参数3...]
-String 
+//有序可选参数
+  printSomething(int a, int b, [int c = 10, int d = 20]) {
+    // 函数体
+    return a + b + c;
+  }
 
+  print(printSomething(1, 2, 4));
 
+// 定义可选参数的函数 注意可选参数放在末尾用 [参数1，参数2，参数3...]
+  String printuserinfowithwithParam(String usnerName,
+      {String sex = '', String hob = ''}) {
+    if (sex.isEmpty) {
+      return '姓名：$usnerName---性别:保密';
+    }
+    return '姓名：$usnerName---性别:$sex------爱好:$hob';
+  }
+
+  print(printuserinfowithwithParam(
+    '章三',
+    hob: '游泳',
+    sex: '男',
+  ));
+
+  void printSomething2(int a, int b, {int c = 99, int d = 100}) {
+    print(a + b + c + d);
+  }
+
+  printSomething2(2, 3, c: 10, d: 11);
+
+  //实现一个 把方法当做参数的方法
+  function1() {
+    print('我是参数函数');
+  }
+
+  void fun(fn) {
+    function1();
+  }
+
+  fun(function1);
 }
